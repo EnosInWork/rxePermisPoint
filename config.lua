ccbc59071973c31ce2e7e1c1a24dd229c831b9bc
Five-Devv <haxy60@gmail.com>
2022-01-19 21:57:22 +0100

    up

--- config.lua
+++ config.lua
@@ -3,6 +3,6 @@ Config = {
         {jobName = 'police', posMenu = vector3(443.53, -975.0, 30.69)},
     --  {jobName = 'sheriff', posMenu = vector3(2495.495, -425.207, 99.11)}
     },
-    permisdecision = false -- true pour regrouper les 3 permis (voiture, moto, camion) en 1 /////// false pour avoir 3 permis différent (voiture, moto, camion)
+    permisdecision = false, -- true pour regrouper les 3 permis (voiture, moto, camion) en 1 /////// false pour avoir 3 permis différent (voiture, moto, camion)
     cmdplayer = "permis" -- /permis pour ouvrir le menu voir les points
 }
